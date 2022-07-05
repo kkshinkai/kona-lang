a1ca0af39948d690a9a42c9162908ae28fc251d4
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-07-05 23:23:40 +0800

    Create token definitions

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,9 +1,4 @@
 // Copyright (c) Kk Shinkai. All Rights Reserved. See LICENSE.txt in the project
 // root for license information.
 
-mod span;
-mod ast;
-
-fn main() {
-    println!("Hello, world!");
-}
+pub mod token;
