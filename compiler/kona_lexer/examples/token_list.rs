d3c39986e1ebd0ea06251ce98d9d6bcb0c751dd3
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-07-10 13:20:37 +0800

    Initialize lexer examples

---
+++ compiler/kona_lexer/examples/token_list.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Kk Shinkai. All Rights Reserved. See LICENSE.txt in the project
+// root for license information.
+//
+// $ cargo run --package kona_lexer --example token_list
+
+fn main() {
+    println!("Hello, token_list!");
+}
