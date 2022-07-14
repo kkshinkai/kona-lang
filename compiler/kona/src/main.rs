ac2991343c582c41c19b6a0ab7e33f70409b7dee
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-07-14 12:59:01 +0800

    Remove some legacy code

--- compiler/kona/src/main.rs
+++ compiler/kona/src/main.rs
@@ -1,10 +1,6 @@
 // Copyright (c) Kk Shinkai. All Rights Reserved. See LICENSE.txt in the project
 // root for license information.
 
-mod span_legacy;
-mod ast;
-mod parse;
-
 fn main() {
     println!("Hello, world!");
 }
